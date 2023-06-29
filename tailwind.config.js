c1d9af29357e49b86b70a65332d5ae044819949d
pushkar-28 <kharepushkar2804@gmail.com>
2023-06-30 00:21:40 +0530

    main section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        bgBottom: "url(assets/backgroundBottom.svg) !important",
+        bgImg: "url(assets/background.svg) !important",
+      },
+      colors: {
+        accent: "#FB7306",
+      },
+      height: {
+        topDiv: "709px",
+        bottomImg: "790px",
+        screen: "1495px",
+      },
+      width: {
+        subText: "614px",
       },
     },
   },
   plugins: [],
-}
+};
