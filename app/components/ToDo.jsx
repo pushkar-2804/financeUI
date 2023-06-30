b3bddfe189888f6e2b2d7ebf07f453f746be8ccb
pushkar-28 <kharepushkar2804@gmail.com>
2023-06-30 14:01:39 +0530

    todo

---
+++ app/components/ToDo.jsx
@@ -0,0 +1,117 @@
+"use client";
+import { useState } from "react";
+
+const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [searchedTerm, setSearchedTerm] = useState("");
+
+  const addTasks = () => {
+    if (task !== "") {
+      setTasks([...tasks, { text: task, completed: false }]);
+      setTask("");
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchedTerm(e.target.value);
+  };
+
+  const deleteTasks = (index) => {
+    const updatedList = [...tasks];
+    updatedList.splice(index, 1);
+    setTasks(updatedList);
+  };
+
+  const toggleComplete = (index) => {
+    const updatedList = [...tasks];
+    updatedList[index].completed = !updatedList[index].completed;
+    setTasks(updatedList);
+  };
+
+  const filteredTodos = tasks.filter((todo) =>
+    todo.text.toLowerCase().includes(searchedTerm.toLowerCase())
+  );
+
+  return (
+    <div className="flex flex-col items-center py-10 bg-bgImg">
+      <h1 className="text-4xl m-16 font-bold">The Todo App</h1>
+      <div className="p-6 flex flex-row">
+        <input
+          className="flex-grow rounded-md p-4 mr-4 text-black"
+          type="text"
+          value={task}
+          onChange={(e) => {
+            setTask(e.target.value);
+          }}
+          placeholder="Create a new todo"
+        />
+        <input
+          type="text"
+          className="w-full border rounded py-2 px-4  transition duration-300 ease-in-out hover:bg-gray-200 hover:shadow-md text-black"
+          placeholder="Search Todo"
+          value={searchedTerm}
+          onChange={handleSearch}
+        />
+        <button
+          onClick={addTasks}
+          className="bg-red-500 text-white p-4 ml-3 rounded-md font-bold hover:bg-green-600"
+        >
+          Add Tasks
+        </button>
+      </div>
+      <div>
+        {filteredTodos?.length > 0 ? (
+          <ul>
+            {filteredTodos.map((task, index) => (
+              <div
+                className="flex bg-slate-100 m-4 py-4 pl-12 pr-4 rounded-md text-black"
+                key={index}
+              >
+                <li
+                  className={`self-center font-semibold pr-10 mr-6 ${
+                    task.completed ? "text-gray-500 line-through" : ""
+                  }`}
+                >
+                  {task.text}
+                </li>
+                <button
+                  onClick={() => {
+                    deleteTasks(index);
+                  }}
+                  className="bg-red-500 text-white p-2 mx-1 rounded-md font-bold hover:bg-red-600"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    toggleComplete(index);
+                  }}
+                  className={`bg-green-500 text-white p-2 mx-1 rounded-md font-bold  ${
+                    task.completed
+                      ? "bg-blue-600  pointer-events-none"
+                      : " hover:bg-green-700"
+                  }`}
+                  style={{
+                    cursor: task.completed ? "not-allowed" : "pointer",
+                    opacity: task.completed ? 0.5 : 1,
+                  }}
+                >
+                  Complete
+                </button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            <p className="self-center font-semibold pr-10 mr-6 grow">
+              No Task Found
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
--- app/page.js
+++ app/page.js
@@ -2,96 +2,104 @@ import Image from "next/image";
 import img from "./assets/phone.svg";
 import bgBtm from "./assets/backgroundBottom.svg";
 import iconImg from "./assets/iconImg.svg";
+import Todo from "./components/ToDo";
 export default function Home() {
   return (
-    <section className="h-screen bg-bgImg w-full overflow-hidden">
-      <main className="flex flex-col items-center justify-between pt-24 h-[569]">
-        {/* Header */}
-        <div className="flex flex-col items-center justify-center">
-          <h3 className="text-7xl text-white font-semibold ">
-            Check your financial health
-          </h3>
-          <p className="leading-tight text-slate-300 text-3xl w-subText text-center pt-6">
-            Use WeathoMeter to get a free report card for your finances- within
-            minutes!
-          </p>
-          <button className="btn bg-accent mt-10 px-6 py-2 rounded-3xl text-3xl">
-            Get Started
-          </button>
-        </div>
-        {/* List */}
-        <div className="flex justify-between mt-10">
-          <ul className="text-3xl list-inside mt-28 ">
-            <li className="mb-10">
-              Expected Retirement Age <br />
-              <hr
-                width={"95%"}
-                className="mt-4 relative left-12 h-0.5 bg-white"
-              />
-            </li>
-            <li>
-              Identify Mistakes
-              <br />
-              <hr
-                width={"95%"}
-                className="mt-4 relative left-12 h-0.5 bg-white"
-              />
-            </li>
-          </ul>
+    <>
+      <section className="bg-bgImg w-full overflow-scroll">
+        <main className="flex flex-col items-center justify-between pt-24 relative">
+          {/* Header */}
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-7xl text-white font-semibold ">
+              Check your financial health
+            </h3>
+            <p className="leading-tight text-slate-300 text-3xl w-subText text-center pt-6">
+              Use WeathoMeter to get a free report card for your finances-
+              within minutes!
+            </p>
+            <button className="btn bg-accent mt-10 px-6 py-2 rounded-3xl text-3xl">
+              Get Started
+            </button>
+          </div>
+          {/* List */}
+          <div className="flex justify-between mt-10">
+            <ul className="text-3xl list-inside mt-28 designList">
+              <li className="mb-10 list">
+                Expected Retirement Age <br />
+                <hr
+                  width={"95%"}
+                  className="mt-4 relative left-12 h-0.5 bg-white"
+                />
+              </li>
+              <li className="list">
+                Identify Mistakes
+                <br />
+                <hr
+                  width={"95%"}
+                  className="mt-4 relative left-12 h-0.5 bg-white"
+                />
+              </li>
+            </ul>
+            <Image
+              src={img}
+              alt="My Image"
+              width={525}
+              height={790}
+              className="relative left-16"
+            />
+            <ul className="text-3xl list-inside mt-28 relative -left-12 designList">
+              <li className="mb-10 list">
+                Personalised Road Map <br />
+                <hr
+                  width={"95%"}
+                  className="mt-4 relative left-12 h-0.5 bg-white"
+                />
+              </li>
+              <li className="mb-10 list">
+                Tips To Improve <br />
+                <hr
+                  width={"95%"}
+                  className="mt-4 relative left-12 h-0.5 bg-white"
+                />
+              </li>
+            </ul>
+          </div>
           <Image
-            src={img}
-            alt="My Image"
-            width={525}
+            src={bgBtm}
+            width={1526}
             height={790}
-            className="relative left-16"
+            alt="bg"
+            className="wave"
           />
-          <ul className="text-3xl list-inside mt-28 relative -left-12">
-            <li className="mb-10">
-              Personalised Road Map <br />
-              <hr
-                width={"95%"}
-                className="mt-4 relative left-12 h-0.5 bg-white"
-              />
-            </li>
-            <li className="mb-10">
-              Tips To Improve <br />
-              <hr
-                width={"95%"}
-                className="mt-4 relative left-12 h-0.5 bg-white"
-              />
-            </li>
-          </ul>
-        </div>
-      </main>
+        </main>
 
-      {/* Footer */}
-      <div>
-        <div className="wave">
-          <Image src={bgBtm} width={1526} height={790} alt="bg" className="" />
+        {/* Footer */}
+        <div className="relative h-[568]">
+          <footer className="pb-8 pt-10 flex flex-col items-center text-center relative z-30">
+            <h3 className="text-6xl text-white font-semibold relative top-10">
+              How it works?
+            </h3>
+            <div className="flex justify-center">
+              <Image src={iconImg} width={497} height={884} alt="icons" />
+            </div>
+            <div className="flex text-3xl relative -top-16 left-12">
+              <span className="px-4">
+                Answer few <br /> questions
+              </span>
+              <span className="px-4">
+                Register using <br /> phone and OTP
+              </span>
+              <span className="px-4 ">
+                Get report and <br /> personal roadmap
+              </span>
+            </div>
+            <button className="btn bg-accent my-4 px-6 py-2 rounded-3xl text-3xl">
+              Get Started
+            </button>
+          </footer>
         </div>
-        <footer className="pt-8 flex flex-col items-center text-center relative z-30">
-          <h3 className="text-6xl text-white font-semibold relative top-10">
-            How it works?
-          </h3>
-          <div className="flex justify-center">
-            <Image src={iconImg} width={497} height={884} alt="icons" />
-          </div>
-          <div className="flex text-3xl relative -top-16 left-12">
-            <span className="px-4">
-              Answer few <br /> questions
-            </span>
-            <span className="px-4">
-              Register using <br /> phone and OTP
-            </span>
-            <span className="px-4 ">
-              Get report and <br /> personal roadmap
-            </span>
-          </div>
-          <button className="btn bg-accent mt-4 px-6 py-2 rounded-3xl text-3xl">
-            Get Started
-          </button>
-        </footer>
-      </div>
-    </section>
+      </section>
+      <Todo />
+    </>
   );
 }
